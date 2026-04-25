187f572f96514a314606d289baf5439388d27335
Lukas-Kubinec <luko.kubinec@gmail.com>
2026-04-25 19:19:52 +0100

    wip

--- Assets/Scripts/PickUpEffects.cs
+++ Assets/Scripts/PickUpEffects.cs
@@ -3,40 +3,110 @@ using UnityEngine.UIElements;
 
 public class PickUpEffects : MonoBehaviour
 {
+    [Header("Rotation")]
     public float rotationSpeed = 10f;
-    public float updownMovementSpeed = 2f;
-    public float newPositionOffset = 2f;
-    private Vector3 startLocalPos;
-    private Vector3 endLocalPos;
-    private bool isMovingUp = true;
+
+    [Header("Material")]
+    public Color mainColour;
+    public Color secColour;
+    private bool swapFromMainToSec = true;
+    private float lerpColourAmount = 0.0f;
+    private Material material;
+    private Renderer matRenderer;
+
+    [Header("Up&Down movement")]
+    public float moveUpBy = 1.5f;
+    private float lerpMoveAmount = 0.0f;
+    private Vector3 startPos;
+    private Vector3 endPos;
+    private bool moveUp = true;
 
     private void Start()
     {
-        startLocalPos = transform.position;
-        endLocalPos = new(startLocalPos.x, startLocalPos.y + newPositionOffset, startLocalPos.z);
+        // Up&Down movement
+        startPos = transform.position;
+        endPos = transform.position;
+        endPos.y += moveUpBy; // pushes the end position up by X amount
+
+        // Colour change
+        matRenderer = GetComponent<Renderer>();
+        material = matRenderer.material;
     }
 
     // Update is called once per frame
     void Update()
     {
         RotateAround();
-        MoveUpDown();
+        HandleColourSwap();
+        HandleUpDown();
     }
 
-    void MoveUpDown()
+    // Up & Down movement
+    void HandleUpDown()
     {
-        if (transform.position == startLocalPos) { isMovingUp = true; }
-        if (transform.position == endLocalPos) { isMovingUp = false; }
+        if (transform.position == startPos)
+        {
+            moveUp = true;
+            lerpMoveAmount = 0.0f; // Resets the movement amount
+        } else if (transform.position == endPos)
+        {
+            moveUp = false;
+            lerpMoveAmount = 0.0f; // Resets the movement amount
+        }
 
-        if (isMovingUp)
+        if (moveUp)
         {
-            transform.position = Vector3.Lerp(startLocalPos, endLocalPos, Time.deltaTime * updownMovementSpeed);
-        } else
+            // Moves up
+            MoveUpDown(startPos, endPos);
+        } 
+        else
         {
-            transform.position = Vector3.Lerp(endLocalPos, startLocalPos, Time.deltaTime * updownMovementSpeed);
+            // Moves down
+            MoveUpDown(endPos, startPos);
         }
     }
 
+
+    void MoveUpDown(Vector3 from, Vector3 to)
+    {
+        lerpMoveAmount += Time.deltaTime;
+        transform.position = Vector3.Lerp(from, to, Mathf.SmoothStep(0.0f, 1.0f, lerpMoveAmount));
+    }
+
+    // Colour change
+    void HandleColourSwap()
+    {
+
+        if (material.GetColor("_BaseColor") == mainColour)
+        {
+            swapFromMainToSec = true;
+            lerpColourAmount = 0.0f; // Resets the lerp amount
+        }
+        else if (material.GetColor("_BaseColor") == secColour)
+        {
+            swapFromMainToSec = false;
+            lerpColourAmount = 0.0f; // Resets the lerp amount
+        }
+
+
+        if (swapFromMainToSec)
+        {
+            ShiftColour(mainColour, secColour);
+        }
+        else
+        {
+            ShiftColour(secColour, mainColour);
+        }
+    }
+
+    void ShiftColour(Color from, Color to)
+    {
+        lerpColourAmount += Time.deltaTime;
+        var newColour = Color.Lerp(from, to, Mathf.SmoothStep(0.0f, 1.0f, lerpColourAmount));
+        material.SetColor("_BaseColor", newColour);
+    }
+
+    // Object rotation
     void RotateAround()
     {
         transform.Rotate(rotationSpeed * Time.deltaTime * Vector3.up);
